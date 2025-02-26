1aa62b11e3b93bc04536134c3058d0d7b46fcc05
shimhwayoung <g09038@naver.com>
2025-02-26 14:52:58 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,19 +1,16 @@
 radio.onReceivedNumber(function (receivedNumber) {
     이동방향 = receivedNumber
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-})
 let 이동방향 = 0
 maqueenPlusV2.I2CInit()
-radio.setGroup(129)
+radio.setGroup(1)
 basic.forever(function () {
     if (이동방향 == 1) {
         maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 10)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
     } else if (이동방향 == 2) {
         maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Backward, 10)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
     } else {
     	
     }
